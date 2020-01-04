dc0674aaef3c58493df470138dad62748f313787
AlexCsanyi <alex.csanyi@gmail.com>
2020-01-04 13:34:39 +0000

    fixed depreciation issues, IP address err

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require("express");
+const connectDB = require("./config/db");
+
 const app = express();
+
+// connect database
+connectDB();
+
 app.get("/", (req, res) => res.send("Hello World!"));
 const port = process.env.PORT || 8082;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
