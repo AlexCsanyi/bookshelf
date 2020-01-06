fb6d6420227afbb444ab6b70a183d46f645c790d
AlexCsanyi <alex.csanyi@gmail.com>
2020-01-06 07:30:08 +0000

    updated css and created build folder

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const connectDB = require("./config/db");
 var cors = require("cors");
-const path = require("path");
 
 // routes
 const books = require("./routes/api/books");
+
 const app = express();
 
-// connect database
+// Connect Database
 connectDB();
 
 // cors
@@ -16,22 +16,11 @@ app.use(cors({ origin: true, credentials: true }));
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.send("Hello world!"));
 
 // use Routes
-app.use("/", express.static(path.join(__dirname, "/bookshelf/build")));
-
-// Serve static assets if in production
-
-if (process.env.NODE_ENV === "production") {
-  // set static folder
-  app.use("/", express.static(path.join(__dirname, "/bookshelf/build")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "bookshelf", "build", "index.html"));
-  });
-}
+app.use("/api/books", books);
 
 const port = process.env.PORT || 8082;
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.listen(port, () => console.log(`Server running on port ${port}`));
