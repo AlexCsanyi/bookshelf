5410038db93d66370ac290de7c33953cd3b4ca47
AlexCsanyi <alex.csanyi@gmail.com>
2020-01-06 05:40:03 +0000

    added necessary changes for heroku

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
 var cors = require("cors");
+const path = require('path')
 
 // routes
 const books = require("./routes/api/books");
@@ -21,4 +22,13 @@ app.get("/", (req, res) => res.send("Hello World!"));
 app.use("/api/books", books);
 
 const port = process.env.PORT || 8082;
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static( 'bookshelf/build' ));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'bookshelf', 'build', 'index.html'))
+    })
+}
+
 app.listen(port, () => console.log(`Server is running on port ${port}`));
