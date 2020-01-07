90a31d60f33564a3e2c7f57af04244ea089951fe
AlexCsanyi <alex.csanyi@gmail.com>
2020-01-07 05:07:51 +0000

    deploying

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
 var cors = require("cors");
+const path = require("path");
 
 // routes
 const books = require("./routes/api/books");
@@ -16,10 +17,20 @@ app.use(cors({ origin: true, credentials: true }));
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.send("Hello world!"));
+// app.get("/", (req, res) => res.send("Hello world!"));
 
 // use Routes
 app.use("/api/books", books);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("bookshelf/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "bookshelf", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 8082;
 
